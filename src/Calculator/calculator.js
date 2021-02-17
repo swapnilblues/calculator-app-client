de4c2798381aca51a34409a8127e66680bad9298
Swapnil Bhaumik <swapnilbhaumikcfc@gmail.com>
2021-02-18 02:16:30 +0530

    Client message service with socket working

---
+++ src/Calculator/calculator.js
@@ -0,0 +1,85 @@
+import React from "react";
+import io from 'socket.io-client'
+import TextField from '@material-ui/core/TextField'
+
+const socket = io('http://localhost:4000')
+
+export default class Calculator extends React.Component {
+
+    state = {
+        message: '',
+        name: '',
+        chat : [],
+        nameEntered: false
+    }
+
+    componentDidMount() {
+        this.setState({
+            name: this.props.name
+        })
+        this.updateChat()
+    }
+
+    updateChat = () => {
+        socket.on('a', ({ name, message }) => {
+            this.setState({
+                chat: [...this.state.chat, {name, message}]
+            })
+        })
+    }
+
+    renderChat = () => {
+        return this.state.chat.map(({ name, message }, index) => (
+            <div key={index}>
+                <h3>
+                    {name}: <span>{message}</span>
+                </h3>
+            </div>
+        ))
+    }
+
+    onMessageSubmit = async (e) => {
+        e.preventDefault()
+        await socket.emit('a', this.state.name, this.state.message)
+        await this.setState({
+            message: ''
+        })
+    }
+    render() {
+        return (
+            <div className="card">
+                <form onSubmit={this.onMessageSubmit}>
+                    <h1>Calculator</h1>
+                    <div className="name-field">
+                        <TextField
+                            name="name"
+                            value={this.state.name}
+                            disabled
+                            label="Name"
+                        />
+                    </div>
+                    <div>
+                        <TextField
+                            name="message"
+                            onChange={async (e) =>
+                                await this.setState({
+                                    message: e.target.value
+                                })
+                            }
+                            value={this.state.message}
+                            id="outlined-multiline-static"
+                            variant="outlined"
+                            label="Message"
+                        />
+                    </div>
+                    <button>Send Message</button>
+                </form>
+                <div className="render-chat">
+                    <h1>Calculation Log</h1>
+                    {this.renderChat()}
+                </div>
+            </div>
+        )
+    }
+}
+
---
+++ src/entry.js
@@ -0,0 +1,49 @@
+import React from "react";
+import TextField from '@material-ui/core/TextField'
+import Calculator from "./Calculator/calculator";
+
+export default class Entry extends React.Component {
+
+    state = {
+        name: '',
+        submit: false
+    }
+
+    submit = () => {
+        this.setState({
+            submit: true
+        })
+    }
+    render() {
+        return (
+           <div>
+               {   !this.state.submit &&
+               <div className="login-form left-margin">
+                   <form className="bg-color">
+                       <h1>Calculator</h1>
+                       <div className="name-field">
+                           <TextField
+                               onChange={async (e) =>
+                                   await this.setState({
+                                       name: e.target.value
+                                   })
+                               }
+                               value={this.state.name}
+                           />
+                       </div>
+                       <button onClick={this.submit}>Enter</button>
+                   </form>
+               </div>
+               }
+               { this.state.submit &&
+                 <Calculator
+                    name = {this.state.name}
+                 />
+                }
+           </div>
+
+
+        )
+    }
+}
+
